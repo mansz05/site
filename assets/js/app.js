13cad0cf8a82d4608bb477d759203a90845f9c65
mansz05 <mansziesel@gmail.com>
2021-07-13 21:22:55 +0200

    custom scrollbar toegevoegd

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,3 +2,9 @@
 particlesJS.load('particles-js', 'assets/particles.json', function() {
   console.log('callback - particles.js config loaded');
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+  //The first argument are the elements to which the plugin shall be initialized
+  //The second argument has to be at least a empty object or a object with your desired options
+  OverlayScrollbars(document.querySelectorAll('body'), { });
+});
